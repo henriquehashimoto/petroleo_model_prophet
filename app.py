34ffc5e140dbf27e32b40a7a210a346171a5ea6d
ronecil05 <66749573+ronecil05@users.noreply.github.com>
2024-01-24 00:08:14 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from utils import formaterData, formaterUniqueid, modelo
+from utils import formaterData, formaterUniqueid
 from sklearn.pipeline import Pipeline
 import joblib
 from joblib import load
@@ -11,10 +11,13 @@ from prophet import Prophet
 from prophet.plot import plot_plotly, plot_components_plotly
 from plotly import graph_objs as go
 
-#
 
 #carregando os dados 
 dados = pd.read_csv('./dados/df.csv')
+dados = dados.loc[(dados['ds'] >= '2020-01-01')]
+
+#print(dados.tail())
+
 dados_linha = dados.copy()
 
 dados_linha['ds'] = pd.to_datetime(dados_linha['ds'], format="%Y-%m-%d")
@@ -26,16 +29,11 @@ st.markdown("<h1 style='text-align: center; '> Previsão de Petróleo - Modelo P
 st.warning(f"Obs: A última data da base de dados é : {str(dados_linha['ds'].max())[:10]}")
 
 
-#dateFinal = st.date_input("Prencha a data que quer que seja previsto")
-#dateFinalTrat = pd.to_datetime(dateFinal, format="%Y-%m-%d")
-#quantidade_dias = abs((dateFinalTrat - dados_linha['ds'].max()).days)
+dateFinal = st.date_input("Prencha a data final da previsão")
+dateFinalTrat = pd.to_datetime(dateFinal, format="%Y-%m-%d")
+quantidade_dias = abs((dateFinalTrat - dados_linha['ds'].max()).days)
 
-quantidade_dias = st.slider("Quantidade de dias de previsão (5 anos - em dias)", 30, 1825)
-
-
-
-#print(dateFinalTrat)
-
+#quantidade_dias = st.slider("Quantidade de dias de previsão (1 ano - em dias)", 30, 365)
 
 
 
@@ -52,10 +50,6 @@ df_pipe = pipeline(dados)
 #print(df_pipe.head())
 
 if st.button('Enviar'):
-    #df_result = modelo.prophet(quantidade_dias, df_pipe)
-    #print("apertou o botao")
-    #print(df_result.head())
-  
         df_pipe = df_pipe.drop('unique_id', axis=1)
 
         #Treina
@@ -67,16 +61,18 @@ if st.button('Enviar'):
 
         # Unindo o forecast usando a previsão futura
         forecast = modelo_prophet.predict(future)
-        forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+        forecast = forecast.round(2)
+        #print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].head())
 
         df_result = forecast.reset_index().merge(df_pipe, on=['ds'], how='left')
 
-        fig1 = modelo_prophet.plot(forecast)
+        fig1 = modelo_prophet.plot(forecast,xlabel="Data",ylabel="Preço")
 
         st.write(fig1)
 
         #grafico
-        grafico1 = plot_plotly(modelo_prophet,forecast)
+        grafico1 = plot_plotly(modelo_prophet,forecast,xlabel="Data",ylabel="Preço")
         st.plotly_chart(grafico1)
 
         grafico2 = plot_components_plotly(modelo_prophet,forecast)
